fb4545972ecd29316e223b484d49ff493c707097
raufsamestone <berkaydemirbas@gmail.com>
2023-03-19 17:33:48 +0300

    added randomly adjusting speed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
-import React, { useRef } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { Canvas, useFrame } from "react-three-fiber";
 import * as THREE from "three";
 
-const Box = () => {
+const Box = ({ speed }) => {
   const meshRef = useRef();
 
   useFrame(() => {
-    meshRef.current.rotation.x += 0.01;
-    meshRef.current.rotation.y += 0.01;
+    meshRef.current.rotation.x += speed / 100;
+    meshRef.current.rotation.y += speed / 100;
   });
 
   return (
@@ -19,12 +19,38 @@ const Box = () => {
 };
 
 const Home = () => {
+  const [speed, setSpeed] = useState(0);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `https://jsonplaceholder.typicode.com/todos/${getRandomInt(1, 200)}`
+        );
+        const data = await response.json();
+        setSpeed(data.title.length / 10);
+        console.log(data.title.length);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    const getRandomInt = (min, max) => {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
+
+    const intervalId = setInterval(() => {
+      fetchData();
+    }, 2000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div>
       <Canvas className="canvas">
         <ambientLight intensity={2.5} />
         <pointLight position={[10, 10, 10]} />
-        <Box />
+        <Box speed={speed} />
       </Canvas>
       <style>{`
         .canvas {
